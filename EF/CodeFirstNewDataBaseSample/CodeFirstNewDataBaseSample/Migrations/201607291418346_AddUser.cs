9094e669c2aa739d9e060ca6388be5d39b559ad0
YKShtykov <yakov.shtykov@gmail.com>
2016-07-31 19:01:56 +0300

    Initial commit

---
+++ EF/CodeFirstNewDataBaseSample/CodeFirstNewDataBaseSample/Migrations/201607291418346_AddUser.cs
@@ -0,0 +1,53 @@
+namespace CodeFirstNewDataBaseSample.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUser : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Blogs",
+                c => new
+                    {
+                        BlogId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        URL = c.String(),
+                    })
+                .PrimaryKey(t => t.BlogId);
+            
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        PostId = c.Int(nullable: false, identity: true),
+                        PostTitle = c.String(),
+                        PostContent = c.String(),
+                        BlogId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.PostId)
+                .ForeignKey("dbo.Blogs", t => t.BlogId, cascadeDelete: true)
+                .Index(t => t.BlogId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Username = c.String(nullable: false, maxLength: 128),
+                        DisplayName = c.String(),
+                    })
+                .PrimaryKey(t => t.Username);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Posts", "BlogId", "dbo.Blogs");
+            DropIndex("dbo.Posts", new[] { "BlogId" });
+            DropTable("dbo.Users");
+            DropTable("dbo.Posts");
+            DropTable("dbo.Blogs");
+        }
+    }
+}
---
+++ EF/CodeFirstNewDataBaseSample/CodeFirstNewDataBaseSample/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstNewDataBaseSample
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         using (var bc = new BlogContext())
+         {
+            Console.WriteLine("Enter name");
+            var name = Console.ReadLine();
+
+            var blog = new Blog { Name = name };
+            bc.Blogs.Add(blog);
+            bc.SaveChanges();
+
+            var query = bc.Blogs.OrderBy(b=>b.Name).Select(b=>b);
+
+            foreach (var item in query)
+            {
+               Console.WriteLine(item.Name);
+            }
+         }
+         Console.ReadKey();
+      }
+   }
+
+   public class Blog
+   {
+      public int BlogId { get; set; }
+      public string Name { get; set; }
+      public string  URL { get; set; }
+
+      public virtual List<Post> Posts { get; set; }
+   }
+
+   public class Post
+   {
+      public int PostId { get; set; }
+      public string PostTitle { get; set; }
+      public string PostContent { get; set; }
+
+      public int BlogId { get; set; }
+      public virtual Blog Blog { get; set; }
+   }
+
+   public class BlogContext: DbContext
+   {
+      public DbSet<Blog> Blogs { get; set; }
+      public DbSet<Post> Posts { get; set; }
+      public DbSet<User> Users { get; set; }
+   }
+
+   public class User
+   {
+      [Key]
+      public string Username { get; set; }
+      public string DisplayName { get; set; }
+   }
+}
